7d2c6ad6d0cdbe06901a2ac20d73aeb4b089fa0c
ThaiSonDev <sonthaidev91@gmail.com>
2023-03-28 23:52:24 +0700

    install the necessary package for the project

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require("./src/app");
+
+const PORT = 3055;
+
+const server = app.listen(PORT, () => {
+  console.log(`WSV eCommerce start with ${PORT}`);
+});
+
+// process.on("SIGINT", () => {
+//   server.close(() => console.log("Exit Server Express"));
+//   // notify.send(ping...)
+// });
---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+// const { compile } = require("morgan");
+const app = express();
+const compression = require("compression");
+
+//init middlewares
+app.use(morgan("dev")); //dau ra ngan gon, trang thai code duoc to mau
+// app.use(morgan("combined")); //theo tc apache Jappan
+// app.use(morgan("common"));
+// app.use(morgan("short"));
+// app.use(morgan("tiny"));
+app.use(helmet()); //hacker tham do, xem header he thong trong curl
+//X-Powered-By -> tim lo hong express, chua va, duc vao lo hong do ->nen su dung helmet de che dau lai
+app.use(compression()); //giam bang thong 151kB -> ~0B
+//init db
+
+//init routers
+app.get("/", (req, res, next) => {
+  const strCompress = "Hello Fantipjs!";
+  return res.status(200).json({
+    message: "Welcome Fantipjs!",
+    metadata: strCompress.repeat(10000),
+  });
+});
+
+//handling error
+
+module.exports = app;
