8de13c0a91b59741d3d876e46cf843c4b302baba
ThaiSonDev <sonthaidev91@gmail.com>
2023-03-31 22:36:48 +0700

    API signUp Shop p2

--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -7,6 +7,26 @@ const mongoose = require("mongoose");
 // const connectString = `mongodb://localhost:27017/shopDEV`;
 const connectString = `mongodb://${host}:${port}/${name}`;
 
+// const mongoose = require("mongoose");
+
+// mongoose.Promise = global.Promise;
+
+// // Connect MongoDB at default port 27017.
+// mongoose.connect(
+//   "mongodb://localhost:27017/DB Name",
+//   {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//   },
+//   (err) => {
+//     if (!err) {
+//       console.log("MongoDB Connection Succeeded.");
+//     } else {
+//       console.log("Error in DB connection: " + err);
+//     }
+//   }
+// );
+
 // const connectString = `mongodb+srv://thaisondev:thaison91@cluster0.b689s5c.mongodb.net/?retryWrites=true&w=majority`;
 const { countConnect } = require("../helpers/check.connect");
 
---
+++ src/services/access.service.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const shopModel = require("../models/shop.model");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+
+const RoleShop = {
+  SHOP: "SHOP",
+  WRITER: "WRITER",
+  EDITOR: "EDITOR",
+  ADMIN: "ADMIN",
+};
+
+class AccessService {
+  static signup = async ({ name, email, password }) => {
+    try {
+      //step1: check email exists???
+      const holderShop = await shopModel.findOne({ email }).lean();
+
+      if (holderShop) {
+        return {
+          code: "xxxx",
+          message: "Shop already registered!",
+        };
+      }
+
+      const passwordHash = await bcrypt.hash(password, 10);
+      const newShop = await shopModel.create({
+        name,
+        email,
+        password: passwordHash,
+        roles: [RoleShop.SHOP],
+      });
+
+      if (newShop) {
+        //created privateKey, publicKey
+        const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+          modulusLength: 4096,
+        });
+
+        console.log({ privateKey, publicKey }); //save collection KeyStore
+      }
+    } catch (error) {
+      return {
+        code: "xxx",
+        message: error.message,
+        status: "error",
+      };
+    }
+  };
+}
+
+module.exports = AccessService;
